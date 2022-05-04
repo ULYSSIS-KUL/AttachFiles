ULYSSIS-KUL/AttachFiles

commit 8a89c3b74941a390d1b9e903240c3770fb741746

Fix Icons and linkify uploader name

AttachFiles.php
@@ -232,6 +232,7 @@ EOD;
     public static function onBeforePageDisplay($out, $skin) {
         global $wgContentNamespaces;
         global $wgAFIgnoredPages;
+        global $wgExtensionAssetsPath;
 
         $title = $out->getTitle();
         $id = $title->getArticleID();
@@ -273,11 +274,12 @@ EOD;
                     $url = htmlentities($file->getUrl());
                     $displayName = htmlentities($row->displayname);
                     $timestamp = $file->getTimestamp();
+                    $uploader = $file->getUploader();
                     $tableRow = "<tr>";
-                    $tableRow .= "<td><img src=\"extensions/AttachFiles/icons/$fileType.png\" style=\"padding-right: 0.3em\"><a href=\"$url\" download=\"$downloadName\">$displayName</a></td>";
+                    $tableRow .= "<td><img src=\"" . $wgExtensionAssetsPath . "/AttachFiles/icons/$fileType.png\" style=\"padding-right: 0.3em\"><a href=\"$url\" download=\"$downloadName\">$displayName</a></td>";
                     $tableRow .= "<td>" . $context->msg($fileType) . "</td>";
                     $tableRow .= "<td data-sort-value=\"$timestamp\">" . $context->getLanguage()->date($timestamp, true) . "</td>";
-                    $tableRow .= "<td>" . htmlentities($file->getUser()) . "</td>";
+                    $tableRow .= "<td>" . Linker::userLink($uploader->getId(), $uploader->getName()) . "</td>";
                     if ($canDelete) {
                         $articleID = $file->getTitle()->getArticleID();
                         $tableRow .= "<td><a href=\"javascript:deleteFile($articleID, `$displayName`);\">" . $context->msg("attachfiles-delete") . "</a></td>";

commit 372548d34d0a74987357fdbff760ee513589ee33

Add backwards compatibility for MediaWiki <1.37
getUploader() was introduced in MediaWiki 1.37, so commit
8a89c3b74941a390d1b9e903240c3770fb741746 broke the extension for
MediaWiki 1.35 and 1.36

AttachFiles.php
@@ -274,12 +274,19 @@ EOD;
                     $url = htmlentities($file->getUrl());
                     $displayName = htmlentities($row->displayname);
                     $timestamp = $file->getTimestamp();
-                    $uploader = $file->getUploader();
+                    if (method_exists($file, "getUploader")) {
+                        $uploader = $file->getUploader();
+                        $uploaderId = $uploader->getId();
+                        $uploaderName = $uploader->getName();
+                    } else {
+                        $uploaderId = $file->getUser("id");
+                        $uploaderName = $file->getUser("text");
+                    }
                     $tableRow = "<tr>";
                     $tableRow .= "<td><img src=\"" . $wgExtensionAssetsPath . "/AttachFiles/icons/$fileType.png\" style=\"padding-right: 0.3em\"><a href=\"$url\" download=\"$downloadName\">$displayName</a></td>";
                     $tableRow .= "<td>" . $context->msg($fileType) . "</td>";
                     $tableRow .= "<td data-sort-value=\"$timestamp\">" . $context->getLanguage()->date($timestamp, true) . "</td>";
-                    $tableRow .= "<td>" . Linker::userLink($uploader->getId(), $uploader->getName()) . "</td>";
+                    $tableRow .= "<td>" . Linker::userLink($uploaderId, $uploaderName) . "</td>";
                     if ($canDelete) {
                         $articleID = $file->getTitle()->getArticleID();
                         $tableRow .= "<td><a href=\"javascript:deleteFile($articleID, `$displayName`);\">" . $context->msg("attachfiles-delete") . "</a></td>";